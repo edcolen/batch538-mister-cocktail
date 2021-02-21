41316fdce9bbc0e04d1a95404cb4bdd9f206b7b4
Ed Colen <ed.colen@gmail.com>
2021-02-21 14:41:48 -0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+
+  resources :cocktails, shallow: true do
+    resources :doses, only: %i[create destroy]
+    resources :reviews, only: %i[create]
+  end
 end
