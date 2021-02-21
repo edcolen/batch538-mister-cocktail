80e4a3ee809d43d3872b67a1df144240ce399f8c
Ed Colen <ed.colen@gmail.com>
2021-02-21 20:49:52 -0300

    added likes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def like
+    @review = Review.find(params[:id])
+    @review.update(likes: @review.likes += 1)
+    redirect_to cocktail_path(@review.cocktail, anchor: "review-#{@review.id}")
+  end
+
   private
 
   def review_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
+  patch 'cocktails/:id/reviews/:id/like', to: 'reviews#like', as: :like_review
 
   resources :cocktails, shallow: true do
     resources :doses, only: %i[create destroy]
